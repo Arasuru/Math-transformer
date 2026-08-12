a9446405936e92ad5e18dedea7ebce9ae959b506
Arasuru <ravikiranarasur4all@gmail.com>
2026-08-12 10:15:23 +0200

    Continous integration workflow with tsts

---
+++ tests/test_tokenizer.py
@@ -0,0 +1,10 @@
+from src.data.tokenizer import CharTokenizer
+
+def test_tokenizer_encode_decode():
+    tokenizer = CharTokenizer()
+    test_string = "123+456=579"
+
+    encoded = tokenizer.encode(test_string)
+    decoded = tokenizer.decode(encoded)
+
+    assert decoded == test_string, f"Data lost during encoded-decode cycle. Expected: {test_string}, Got: {decoded}"
